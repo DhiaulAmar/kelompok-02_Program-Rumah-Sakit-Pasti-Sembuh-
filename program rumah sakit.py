b41e6a1d6af926e906c06fa992847322695ac9f1
rlaxks <80144755+rlaxks@users.noreply.github.com>
2021-06-15 10:43:03 +0700

    Update program rumah sakit.py
    update tubes

--- program rumah sakit.py
+++ program rumah sakit.py
@@ -250,3 +250,156 @@ while loop:
 
                 tagihan = 150000
                 pay = "Rawat Jalan"
+
+                
+                def strukrawat():
+                    print()
+                    print("-----------------------------------")
+                    print("     Rumah Sakit Pasti Sembuh")
+                    print(" Jl.Bahagia No.76, Jakarta 12930")
+                    print("-----------------------------------")
+                    print()
+                    print("           No Antrian")
+                    print("              ", random.randint(1, 50))
+                    print("          ", poli)
+                    print("          ", dokter)
+                    print("          Pukul", random.choice(jam))
+                    print("  Terima Kasih Atas Kunjungan Anda")
+                    print("        Semoga Lekas Sembuh")
+                    print()
+
+
+                strukrawat()
+
+                bayarnya, byr, jumlah_bayar, kembalian, bayar_apa, nama_pasien, no_bpjs, ttl_pasien, nik, jbayar, Loop = pembayaran()
+                strukbyr(bayarnya, byr, jumlah_bayar, kembalian, bayar_apa, nama_pasien, no_bpjs, ttl_pasien, nik,
+                         jbayar)
+
+
+            elif jenis_rawat == "2":
+                os.system('cls')
+                print("\nPilihan kelas kamar: ")
+                print("\t1. Kelas 3")
+                print("\t2. Kelas 2")
+                print("\t3. Kelas 1")
+                print("\t4. Kelas VIP")
+                pilihan_kamar = input("Silahkan pilih jenis kamar (1/2/3/4): ")
+                dafkam = ["Kelas 3", "Kelas 2", "Kelas 1", "Kelas VIP"]
+                harkam = [200000, 375000, 550000, 725000]
+
+                if pilihan_kamar == "1":
+                    harga_kamar = harkam[0]
+                    print("\nKamar Tersedia")
+                    print("Harga kamar kelas 3 : Rp", harkam[0])
+                    print("Pembayaran hanya untuk DP awal")
+                    print("Silahkan menuju loket untuk melakukan pembayaran awal")
+
+                elif pilihan_kamar == "2":
+                    harga_kamar = harkam[1]
+                    print("\nKamar Tersedia")
+                    print("Harga kamar kelas 2 : Rp", harkam[1])
+                    print("Pembayaran hanya untuk DP awal")
+                    print("Silahkan menuju loket untuk melakukan pembayaran awal")
+
+                elif pilihan_kamar == "3":
+                    harga_kamar = harkam[2]
+                    print("\nKamar Tersedia")
+                    print("Harga kamar kelas 1 : Rp", harkam[2])
+                    print("Pembayaran hanya untuk DP awal")
+                    print("Silahkan menuju loket untuk melakukan pembayaran awal")
+
+                elif pilihan_kamar == "4":
+                    harga_kamar = harkam[3]
+                    print("\nKamar Tersedia")
+                    print("Harga kamar kelas VIP : Rp", harkam[3])
+                    print("Pembayaran hanya untuk DP awal")
+                    print("Silahkan menuju loket untuk melakukan pembayaran awal")
+
+                else:
+                    print("Pilihan kamar tidak tersedia")
+                    sys.exit()
+
+
+                tagihan = harga_kamar
+                pay = "Rawat Inap"
+
+                bayarnya, byr, jumlah_bayar, kembalian, bayar_apa, nama_pasien, no_bpjs, ttl_pasien, nik, jbayar, Loop = pembayaran()
+                strukbyr(bayarnya, byr, jumlah_bayar, kembalian, bayar_apa, nama_pasien, no_bpjs, ttl_pasien, nik,
+                         jbayar)
+
+            ulang = str(input("Apakah anda ingin keluar dari program (Y/T)"))
+            if ulang.upper() == "Y":
+                print("\nTerima Kasih sudah menggunakan jasa rumah sakit kami")
+                print("Semoga Lekas Sembuh")
+                sys.exit()
+            else:
+                continue
+
+
+        elif pilihan_fitur == "3":
+            os.system('cls')
+            print("\nFitur Ambil Antar Obat")
+            print("Silahkan isi data berikut")
+            nama = input("Nama: ")
+            print("Daftar Obat: ")
+            print(" 1. Amoxilin             6. Dexanta              11. Panadol")
+            print(" 2. Alergine             7. Dulcolax             12. Paratusin")
+            print(" 3. Asam Mefenamat       8. Histapan             13. Ranitidine Hexpharm")
+            print(" 4. Cetrizine            9. Imbost force         14. Salbutamol")
+            print(" 5. Combantrin          10. Nalgestan            15. Voltadex")
+            pilobat = input("Pilih obat yang diinginkan (1/2/3...): ")
+            dafobat = ["Amoxilin", "Alergine", "Asam Mefenamat", "Cetrizine ", "Combantrin",
+                       "Dexanta", "Dulcolax", "Histapan", "Imbost force", "Nalgestan",
+                       "Panadol", "Paratusin", "Ranitidine Hexpharm", "Salbutamol", "Voltadex"]
+            harobat = [3700, 44500, 2800, 6000, 14500,
+                       5000, 8500, 8100, 70000, 9000,
+                       10000, 10000, 1600, 1600, 15000]
+
+            if pilobat == "1":
+                obat = dafobat[0]
+                harga_obat = harobat[0]
+            elif pilobat == "2":
+                obat = dafobat[1]
+                harga_obat = harobat[1]
+            elif pilobat == "3":
+                obat = dafobat[2]
+                harga_obat = harobat[2]
+            elif pilobat == "4":
+                obat = dafobat[3]
+                harga_obat = harobat[3]
+            elif pilobat == "5":
+                obat = dafobat[4]
+                harga_obat = harobat[4]
+            elif pilobat == "6":
+                obat = dafobat[5]
+                harga_obat = harobat[5]
+            elif pilobat == "7":
+                obat = dafobat[6]
+                harga_obat = harobat[6]
+            elif pilobat == "8":
+                obat = dafobat[7]
+                harga_obat = harobat[7]
+            elif pilobat == "9":
+                obat = dafobat[8]
+                harga_obat = harobat[8]
+            elif pilobat == "10":
+                obat = dafobat[9]
+                harga_obat = harobat[9]
+            elif pilobat == "11":
+                obat = dafobat[10]
+                harga_obat = harobat[10]
+            elif pilobat == "12":
+                obat = dafobat[11]
+                harga_obat = harobat[11]
+            elif pilobat == "13":
+                obat = dafobat[12]
+                harga_obat = harobat[12]
+            elif pilobat == "14":
+                obat = dafobat[13]
+                harga_obat = harobat[13]
+            elif pilobat == "15":
+                obat = dafobat[14]
+                harga_obat = harobat[14]
+            else:
+                print("Obat Tidak Tersedia")
+                sys.exit()
